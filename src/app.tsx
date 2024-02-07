9bfb56015dada32e327f46ae5369a351b6547db5
henriquebarros <henriquebarros@live.com>
2024-02-07 17:18:12 -0300

    Configurando Eslint e Prettier no projeto

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,12 @@
+import logo from './assets/logo-nlw-expert.svg'
 
 export function App() {
- return <h1>Wello World</h1>
+  return (
+    <div className="mx-auto max-w-6xl my-12">
+      <img src={logo} alt="NLW Expert" />
+      <form>
+        <input type="text" placeholder="Busque em suas notas" />
+      </form>
+    </div>
+  )
 }
-
