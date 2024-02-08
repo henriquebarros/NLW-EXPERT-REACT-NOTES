2c8bc5e6c74401dcb4c177eba3eb314a429f5dac
henriquebarros <henriquebarros@live.com>
2024-02-07 21:04:36 -0300

    Estilização no hover e focus do  Card

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard(){
     return (
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
             <span className='text-sm font-medium text-slate-300'>
                 há 4 dias
             </span>
@@ -8,7 +8,7 @@ export function NoteCard(){
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui saepe nisi, molestias dolor, minus dicta aliquam accusantium, suscipit laborum eligendi culpa repellat distinctio quas perferendis velit sit adipisci praesentium aut. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui saepe nisi, molestias dolor, minus dicta aliquam accusantium, suscipit laborum eligendi culpa repellat distinctio quas perferendis velit sit adipisci praesentium aut.
             </p>
             <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-        </div>
+        </button>
 
     )
 }
